87480c7dc6ab4ad6d76d2f6545f3b485b8c7f644
Umesh Shrestha <stha.ums@gmail.com>
2023-12-26 02:45:21 -0500

    fix: null saftly on cavi decoder

--- lib/dart_e2/formatter/cavi2_transformer.dart
+++ lib/dart_e2/formatter/cavi2_transformer.dart
@@ -40,8 +40,8 @@ class Cavi2Transformer {
       transformed['EE_TIMESTAMP'] = encodedCavi2['time']?['hostTime'];
 
       // Extract metadata values
-      transformed['EE_TOTAL_MESSAGES'] = encodedMetadata['sbTotalMessages'];
-      transformed['EE_MESSAGE_ID'] = encodedMetadata['sbCurrentMessage'];
+      transformed['EE_TOTAL_MESSAGES'] = encodedMetadata?['sbTotalMessages'];
+      transformed['EE_MESSAGE_ID'] = encodedMetadata?['sbCurrentMessage'];
 
       // Process additional data if not a 'notification' or 'heartbeat' event
       if (eEventType == 'payload') {
@@ -49,41 +49,42 @@ class Cavi2Transformer {
 
         // Rename and add capture metadata
         final decodedCaptureMetadata = Map<String, dynamic>.from({});
-        encodedMetadata['captureMetadata']?.forEach((k, v) {
+        encodedMetadata?['captureMetadata']?.forEach((k, v) {
           decodedCaptureMetadata['_C_$k'] = v;
         });
 
         // Rename and add plugin metadata
         final decodedPluginMetadata = Map<String, dynamic>.from({});
-        encodedMetadata['pluginMetadata']?.forEach((k, v) {
+        encodedMetadata?['pluginMetadata']?.forEach((k, v) {
           decodedPluginMetadata['_P_$k'] = v;
         });
 
         // Process data values
-        transformed['STREAM'] = encodedData['identifiers']['streamId'];
-        transformed['INSTANCE_ID'] = encodedData['identifiers']['instanceId'];
-        transformed['ID'] = encodedData['identifiers']['payloadId'];
-        transformed['ID_TAGS'] = encodedData['identifiers']['idTags'];
+        transformed['STREAM'] = encodedData?['identifiers']?['streamId'];
+        transformed['INSTANCE_ID'] = encodedData?['identifiers']?['instanceId'];
+        transformed['ID'] = encodedData?['identifiers']?['payloadId'];
+        transformed['ID_TAGS'] = encodedData?['identifiers']?['idTags'];
 
-        transformed['INITIATOR_ID'] = encodedData['identifiers']['initiatorId'];
-        transformed['SESSION_ID'] = encodedData['identifiers']['sessionId'];
+        transformed['INITIATOR_ID'] =
+            encodedData?['identifiers']?['initiatorId'];
+        transformed['SESSION_ID'] = encodedData?['identifiers']?['sessionId'];
 
         // Process data 'value' and 'specificValue'
-        encodedData['value'].forEach((k, v) {
+        encodedData?['value']?.forEach((k, v) {
           transformed[k.toUpperCase()] = v;
         });
 
-        encodedData['specificValue'].forEach((k, v) {
+        encodedData?['specificValue'].forEach((k, v) {
           transformed[k.toUpperCase()] = v;
         });
 
-        // process time data
-        transformed['TIMESTAMP_EXECUTION'] = encodedData['time'];
+        // process time data : // TODO: handle time when null
+        transformed['TIMESTAMP_EXECUTION'] = encodedData?['time'];
 
         // Process image data
-        String? img = encodedData['img']['id'];
-        int? imgH = encodedData['img']['height'];
-        int? imgW = encodedData['img']['width'];
+        String? img = encodedData?['img']?['id'];
+        int? imgH = encodedData?['img']?['height'];
+        int? imgW = encodedData?['img']?['width'];
 
         if (img != null) {
           transformed['IMG'] = img;
@@ -100,7 +101,7 @@ class Cavi2Transformer {
       }
 
       // Process remaining metadata
-      encodedMetadata.forEach((k, v) {
+      encodedMetadata?.forEach((k, v) {
         transformed[k.toUpperCase()] = v;
       });
 
